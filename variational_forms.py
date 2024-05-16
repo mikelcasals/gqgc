fixed gradient

variational_forms.py
@@ -23,9 +23,7 @@ def perm_equivariant_embedding(A,node_features, betas, gammas):
     for l in range(n_layers):
         for i in range(n_nodes):
             for j in range(i):
-                    # factor of 2 due to definition of gate
-                #a = gammas[l]
-                #temp = np.array(A[i,j], requires_grad=False)
+                # factor of 2 due to definition of gate
                 qml.IsingZZ(2*gammas[l]*A[i,j], wires=[i,j])
 
         for i in range(n_nodes):